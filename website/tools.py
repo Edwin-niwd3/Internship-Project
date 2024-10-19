7597180a6c8e20b76c976f5b3ea554d0b5df74e0
Edwin-niwd3 <edwin.navarro@laverne.edu>
2024-10-19 14:42:54 -0700

    Created more tools to use

--- website/tools.py
+++ website/tools.py
@@ -1,10 +1,14 @@
 from .models import Class
+import difflib
 
 def getNames(Class_Query):
   ans = []
   for Class in Class_Query:
     ans.append(Class.Course_Name)
   return ans
+
+def get_closest_class(level, class_list):
+  return difflib.get_close_matches(level, class_list, n=1)[0] if level else None
 
 def collect_prerequisites(course, collected=None):
     if collected is None:
@@ -19,3 +23,14 @@ def collect_prerequisites(course, collected=None):
         collect_prerequisites(prereq, collected)
     
     return collected
+
+def fetch_course_with_prerequisites(course_name):
+  """This is done to collect all prerequisites for a course by name"""
+  if not course_name:
+    return []
+  course = Class.query.filter_by(Course_Name = course_name).first()
+  if not course:
+    return []
+  prerequisites = collect_prerequisites(course)
+  prerequisites.reverse()
+  return prerequisites
